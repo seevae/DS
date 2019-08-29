d9d3649d9fa74a147aa09e91bc4b08371f7bdfe6
seevae <906435177@qq.com>
2019-08-29 17:37:20 +0800

    树的前中后序遍历非递归

--- BinaryTree/src/main/java/com/qi/BinaryTree.java
+++ BinaryTree/src/main/java/com/qi/BinaryTree.java
@@ -1,6 +1,10 @@
 package com.qi;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BinaryTree {
 
     class TreeNode{
@@ -45,7 +49,7 @@ public class BinaryTree {
             return;
         }
         midOrder(root.left);
-        System.out.println(root.val);
+        System.out.print(root.val+" ");
         midOrder(root.right);
     }
 
@@ -56,8 +60,158 @@ public class BinaryTree {
         }
         afterOrder(root.left);
         afterOrder(root.right);
-        System.out.println(root.val);
+        System.out.print(root.val+" ");
     }
 
-    
+    //求全部节点的个数
+    //总结点个数 = 左子树节点个数 + 右子树节点个数 + 1
+    public int allNode(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        return allNode(root.left)+allNode(root.right)+1;
+    }
+
+    //求叶子节点的个数
+    public int allLeavesNode(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        if(root.left==null && root.right ==null){
+            return 1;
+        }
+
+        return allLeavesNode(root.left)+allLeavesNode(root.right);
+    }
+
+    //求第k层节点个数
+    //假如求第4层. 从第一层算起,k-3后才到达真正统计结点个数的层
+    public int nodesInLineK(TreeNode root,int k){
+        if(root == null){
+            return 0;
+        }else if(k==1){
+            return 1;
+        }
+
+        return nodesInLineK(root.left,k-1)+nodesInLineK(root.right,k-1);
+    }
+
+    //在二叉树中查找指定的值
+    //现在左子树中进行查找, 再到右子树中进行查找
+    public TreeNode findVal(TreeNode root,int val){
+        if(root ==null){
+            return null;
+        }
+
+        if(root.val == val){
+            return root;
+        }
+
+        //在左子树中寻找
+        TreeNode treeNode = findVal(root.left,val);
+        if(treeNode != null){
+            return treeNode;
+        }
+
+        treeNode = findVal(root.right,val);
+        if(treeNode != null){
+            return treeNode;
+        }
+
+        return null;
+    }
+
+    //求树的高度
+    public int getTreeHigh(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        //求左子树的高度
+        int left = getTreeHigh(root.left);
+        int right = getTreeHigh(root.right);
+        return left>right?left+1:right+1;
+    }
+
+    //非递归前序遍历二叉树
+    public void preNode_02(TreeNode root){
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode top = null;
+        while(root != null || !stack.empty()){
+            while(root != null){
+                stack.push(root);
+                System.out.print(root.val+" ");
+                root = root.left;
+            }
+            top = stack.pop();
+            root = top.right;
+        }
+    }
+
+    //非递归中序遍历
+    public void midNode_02(TreeNode root){
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        TreeNode top = null;
+
+        while(cur != null || !stack.empty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            top = stack.pop();
+            System.out.print(top.val+" ");
+            cur = top.right;
+        }
+    }
+
+    //非递归后续遍历
+    public void afterOrder_03(TreeNode root){
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        TreeNode pre = null;
+
+        while(cur!=null || !stack.empty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            cur = stack.peek();
+            if(cur.right == null ||pre.right == null){
+                stack.pop();
+                System.out.print(cur.val+" ");
+                pre = cur;
+                cur = null;
+            }else {
+                cur = cur.right;
+            }
+        }
+    }
+
+    //层序遍历二叉树
+    public void cengxu(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        if(root != null){
+            queue.offer(root);
+        }
+
+        //开始装入左右子树并层序遍历
+        while(!queue.isEmpty()){
+            TreeNode topNode = queue.peek();
+            System.out.print(topNode.val+" ");
+            if(topNode.left != null){
+                queue.offer(topNode.left);
+            }
+            if(topNode.right != null){
+                queue.offer(topNode.right);
+            }
+            queue.poll();
+        }
+    }
+
+
+
 }
--- BinaryTree/src/main/java/com/qi/Test.java
+++ BinaryTree/src/main/java/com/qi/Test.java
@@ -5,10 +5,54 @@ package com.qi;/*
 public class Test {
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
-        //创建树并返回数的根结点
+        //前序遍历 创建树并返回数的根结点
         BinaryTree.TreeNode root = binaryTree.createTree("ABC##DE#G##F###");
 
+        System.out.print("递归前序遍历: ");
         binaryTree.preOrder(root);
+        System.out.println();
+
+        System.out.print("非递归前序遍历: ");
+        binaryTree.preNode_02(root);
+        System.out.println();
+
+        System.out.print("递归中序遍历: ");
+        binaryTree.midOrder(root);
+        System.out.println();
+
+        System.out.print("非递归中序遍历: ");
+        binaryTree.midNode_02(root);
+        System.out.println();
+
+        System.out.print("递归后序遍历: ");
+        binaryTree.afterOrder(root);
+        System.out.println();
+
+        System.out.print("非递归后序遍历: ");
+        binaryTree.afterOrder_03(root);
+        System.out.println();
+
+        System.out.print("层序遍历: ");
+        binaryTree.cengxu(root);
+        System.out.println();
+
+        System.out.print("节点总个数: ");
+        System.out.println(binaryTree.allNode(root));
+
+        System.out.print("叶子节点总个数: ");
+        System.out.println(binaryTree.allLeavesNode(root));
+
+        System.out.print("查找指定的值: ");
+        BinaryTree.TreeNode node = binaryTree.findVal(root,'A');
+        System.out.println(node);
+
+        System.out.print("树的高度: ");
+        int high =binaryTree.getTreeHigh(root);
+        System.out.println(high);
+
+        System.out.print("第3层节点的个数");
+        System.out.println(binaryTree.nodesInLineK(root,3));
+
 
     }
 }
